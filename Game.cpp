6d118ec915ec9494ce0bad4cabec64e924fc3f16
Yeomin Yoon <min050820@gmail.com>
2022-05-31 18:31:56 +0900

    레벨 1~5 구현

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 
 #include "colors.h"
+#include "map.h"
 
 Game::Game() : level(0) {
     clearBoard();
@@ -25,23 +26,29 @@ void Game::run() {
 }
 
 void Game::clearBoard() {
-    for(int i=0; i<24; i++) {
-        for(int j=0; j<24; j++) {
-            board[i][j] = GameCell::EMPTY;
+    for (int i=0; i<24; i++) {
+        for (int j=0; j<3; j++) {
+            unsigned char num = mapData[level][i * 3 + j];
+            for (int k=0; k<8; k++) {
+                if (num & 0x80)
+                    board[i][j * 8 + k] = GameCell::WALL;
+                else
+                    board[i][j * 8 + k] = GameCell::EMPTY;
+                num <<= 1;
+            }
         }
     }
 
-    for(int i=0; i<24; i++) {
-        board[0][i] = GameCell::WALL;
-        board[23][i] = GameCell::WALL;
-        board[i][0] = GameCell::WALL;
-        board[i][23] = GameCell::WALL;
+    for (int i=0; i<24; i++) {
+        for (int j=0; j<24; j++) {
+            int cnt = 0;
+            if ((i == 0 || board[i-1][j] == GameCell::WALL) &&
+                (i == 23 || board[i+1][j] == GameCell::WALL) &&
+                (j == 0 || board[i][j-1] == GameCell::WALL) &&
+                (j == 23 || board[i][j+1] == GameCell::WALL))
+                board[i][j] = GameCell::IMMUNE_WALL;
+        }
     }
-
-    board[0][0] = GameCell::IMMUNE_WALL;
-    board[0][23] = GameCell::IMMUNE_WALL;
-    board[23][0] = GameCell::IMMUNE_WALL;
-    board[23][23] = GameCell::IMMUNE_WALL;
 }
 
 void Game::draw() {
---
+++ map.h
@@ -0,0 +1,6 @@
+#ifndef MAP_H_
+#define MAP_H_
+
+extern unsigned char mapData[5][72];
+
+#endif
